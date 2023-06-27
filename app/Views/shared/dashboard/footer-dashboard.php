e9ce30c3228993f2ec6262ff9784fa368a5b6686
dissidius20 <andayajoshua96@gmail.com>
2023-06-27 16:57:57 +0800

    working dashboard

---
+++ app/Views/shared/dashboard/footer-dashboard.php
@@ -0,0 +1,52 @@
+<footer class="main-footer">
+
+  <div class="float-right d-none d-sm-inline-block">
+  </div>
+</footer>
+<aside class="control-sidebar control-sidebar-dark">
+
+</aside>
+
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"
+  integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ=="
+  crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="//cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/f38a62f9ed.js" crossorigin="anonymous"></script>
+<script src="plugins/jquery/jquery.min.js"></script>
+
+
+<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="plugins/chart.js/Chart.min.js"></script>
+
+<script src="plugins/sparklines/sparkline.js"></script>
+
+<script src="plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+
+<script src="plugins/jquery-knob/jquery.knob.min.js"></script>
+
+<script src="plugins/moment/moment.min.js"></script>
+<script src="plugins/daterangepicker/daterangepicker.js"></script>
+
+<script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+
+<script src="plugins/summernote/summernote-bs4.min.js"></script>
+
+<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+
+<script src="dist/js/adminlte.js?v=3.2.0"></script>
+
+
+<script src="dist/js/pages/dashboard.js"></script>
+</body>
---
+++ app/Views/shared/dashboard/sidenav-dashboard.php
@@ -0,0 +1,30 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+  <a href="#" class="brand-link">
+    <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      style="opacity: .8">
+    <span class="brand-text font-weight-light">Administrator</span>
+  </a>
+
+  <div class="sidebar">
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+        <li class="nav-item">
+          <a href="<?php echo base_url('dashboard'); ?>" class="nav-link">
+            <i class="nav-icon fas fa-tachometer-alt"></i>
+            <p>Dashboard</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="<?php echo base_url('order-list'); ?>" class="nav-link ">
+            <i class="fa-regular fa-rectangle-list order-icon"></i>
+            <p>Order List</p>
+          </a>
+        </li>
+      </ul>
+    </nav>
+
+  </div>
+
+</aside>
